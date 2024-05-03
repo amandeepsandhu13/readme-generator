da7439a07a19ac66a9d26665d6a188aeee491043
Amandeep <samandeepkaur34@gmail.com>
2024-05-02 22:59:03 -0400

    added input fields and init function

---
+++ index.js
@@ -0,0 +1,83 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./Develop/utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'editor',
+        name: 'description',
+        message: 'Enter a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:'
+    },
+    {
+        type: 'editor',
+        name: 'usage',
+        message: 'Enter usage information:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:'
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter your GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('README file created successfully!');
+    });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+     // Use inquirer.prompt to ask the user the array of questions
+     inquirer.prompt(questions)
+     .then((answers) => {
+         // Once user provides answers, generate README content based on answers
+         const readmeContent = generateMarkdown(answers);
+         // Call writeToFile function to write the README file
+         writeToFile('ReadMe.md', readmeContent);
+     })
+     .catch((error) => {
+         console.error(error);
+     });
+}
+
+// Function call to initialize app
+init();
